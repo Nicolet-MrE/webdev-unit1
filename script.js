903098ae5111097cf061847c41f0e077d04bebc2
Neal Easterling | Nicolet High School <jimmy.easterling@nicolet.us>
2026-04-08 14:30:48 -0500

    initial commit

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ window.onload = (e)=>{
 
 document.addEventListener("DOMContentLoaded", (e)=>{
     console.log("DOM loaded!")
+    const sidebar = document.getElementById('sidebar-menu');
+    const menuButton = document.getElementById('ham-menu');
+
+    menuButton.addEventListener('click', ()=>{
+        sidebar.classList.toggle('off-screen');
+        console.log('toggled menu');
+    });   
+
 });
